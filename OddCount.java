9bc7b292eec606f06503b6b3b3dc1982f3dc4a44
KIng-2004 <96736343+KIng-2004@users.noreply.github.com>
2024-02-27 09:09:59 +0530

    Update OddCount.java

--- OddCount.java
+++ OddCount.java
@@ -1,1 +1,9 @@
+import java.util.Scanner;
 
+public class OddCount{
+    public static void main(String[] args) {
+        
+        int a[]=new int[20];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = scanner.nextInt();
