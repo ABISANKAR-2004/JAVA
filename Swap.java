51c4bf4782b8e1e662bb5d30b966575e84fe8bce
KIng-2004 <96736343+KIng-2004@users.noreply.github.com>
2024-02-27 07:28:59 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class Swap{
+    public static void main(String[] args) {
+        
+        int a, n;
+
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        a= scanner.nextInt();
+      System.out.println( "before  swapping  a : "+a+"n:"+n);
+        a=a+n;
+        n=a-n;
+        a=a-n;
+      System.out.println( "after swapping  a : "+a+"n:"+n);
+      
+
+    }} 
+
+
 
