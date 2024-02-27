e23a14bf308f6b5ed675c749b652a334e1c30b16
KIng-2004 <96736343+KIng-2004@users.noreply.github.com>
2024-02-27 09:20:07 +0530

    Update OddCount.java

--- OddCount.java
+++ OddCount.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class OddCount{
     public static void main(String[] args) {
         
-        int a[]=new int[20];
+        
         Scanner scanner = new Scanner(System.in);
+       
         System.out.println("Enter a number: ");
         n = scanner.nextInt();
+         int a[]=new int[n];
+        for (int i=0;i<=a.length-1;i++){
+            a[i]=sc.nextInt();
+        }
+        for (int i=0;i<=a.length-1;i++){
+            System.out.printLn(a[i]);
+        }
