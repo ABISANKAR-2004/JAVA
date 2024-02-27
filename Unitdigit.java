6691a87669bb901bbf114d2b19562d08d8a49d43
KIng-2004 <96736343+KIng-2004@users.noreply.github.com>
2024-02-27 07:18:32 +0530

    Update Unitdigit.java

--- Unitdigit.java
+++ Unitdigit.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class NumberOfDigits {
+    public static void main(String[] args) {
+        // Variables
+        int a=0, n;
+
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        n = scanner.nextInt();
+        while(n>0){
+          n=n/10;
+          a++;
+        }
+        
+
+        // Output
+        System.out.println("Number of digits in the given number: " +a);
+    }}
