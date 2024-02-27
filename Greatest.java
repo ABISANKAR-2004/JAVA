a1fe94c853d423a2d1b02987af4a2cac2d1c4c55
KIng-2004 <96736343+KIng-2004@users.noreply.github.com>
2024-02-27 08:58:21 +0530

    Update Greatest.java

--- Greatest.java
+++ Greatest.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Greatest{
+    public static void main(String[] args) {
+        
+        int a, b,c;
+
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        a = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        b= scanner.nextInt();
+        System.out.println("Enter a number: ");
+        b= scanner.nextInt();
+        System.out.println("Enter a number: ");
+        c= scanner.nextInt();
+        if(a>=b && a>=c)
+          System.out.println("The largest number is a:"+a)
+        else if(b>=a && b>=c)
+            System.out.println("The largest number is b:"+b)
+        else
+            System.out.println("The largest number is c:"+c)
+          }}
